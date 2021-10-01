05e0c685c3e4261ada0203753d2715d2f6b8ab04
Martin <marto7395@gmail.com>
2021-10-01 14:36:55 +0300

    Companies can now have multiple owners

--- UpSkill/Data/Models/Company.cs
+++ UpSkill/Data/Models/Company.cs
@@ -1,33 +1,32 @@
 ﻿namespace UpSkill.Data.Models
 {
-	using System.Collections.Generic;
-	using System.ComponentModel.DataAnnotations;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
 
-	using UpSkill.Data.Common.Models;
+    using UpSkill.Data.Common.Models;
 
-	using static UpSkill.Data.DataConstants.Company;
+    using static UpSkill.Data.DataConstants.Company;
 
-	public class Company : BaseModel<int>
-	{
-		public Company()
-		{
-			this.Employees = new HashSet<Employee>();
-			this.Invoices = new HashSet<Invoice>();
-		}
+    public class Company : BaseModel<int>
+    {
+        public Company()
+        {
+            this.Employees = new HashSet<Employee>();
+            this.Invoices = new HashSet<Invoice>();
+        }
 
-		[Required]
-		public string Name { get; set; }
+        [Required]
+        public string Name { get; set; }
 
-		public string OwnerId { get; set; }
-		public Owner Owner { get; set; }
+        [StringLength(UIC_Length)]
+        public string UIC { get; set; }
 
-		[StringLength(UIC_Length)]
-		public string UIC { get; set; }
+        public string Address { get; set; }
 
-		public string Address { get; set; }
+        public ICollection<Owner> Owner { get; set; }
 
-		public ICollection<Employee> Employees { get; set; }
+        public ICollection<Employee> Employees { get; set; }
 
-		public ICollection<Invoice> Invoices { get; set; }
-	}
+        public ICollection<Invoice> Invoices { get; set; }
+    }
 }
