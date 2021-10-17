8b96de04636667462d1abdfc610788da9dafa46a
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-10-17 16:03:03 +0300

    row 3 done
    #96

---
+++ UpSkill/Infrastructure/UpSkill.Infrastructure.Models/WelcomePage/RequestDemoFromModel.cs
@@ -0,0 +1,26 @@
+﻿namespace UpSkill.Infrastructure.Models.WelcomePage
+{
+    using System.ComponentModel.DataAnnotations;
+    using Microsoft.VisualBasic;
+    using static Common.GlobalConstants.WelcomePageConst;
+
+    public class RequestDemoFromModel
+    {
+        [Required]
+        [MinLength(NameMinLen)]
+        [MaxLength(NameMaxLen)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(CompanyNameMinLen)]
+        [MaxLength(CompanyNameMaxLen)]
+        public string CompanyName { get; set; }
+
+        [Required]
+        [RegularExpression(EmailRegEx)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+    }
+}
