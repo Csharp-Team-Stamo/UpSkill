e9651147bf10946075a057121caaf9daeb4f0897
Yavor Tsanev <69981325+YavorTsanev@users.noreply.github.com>
2021-10-26 12:56:32 +0300

    created EmployeesControler and PostEmployeesCollection
    #123

---
+++ UpSkill/Api/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿namespace UpSkill.Api.Controllers
+{
+    using System.Collections.Generic;
+    using Infrastructure.Models.AddEmployeeModal;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpPost("PostEmployeesCollection")]
+        public ICollection<AddEmployeeFormModel> PostEmployeesCollection(ICollection<AddEmployeeFormModel> employees)
+        {
+            return null;
+        }
+    }
+}
