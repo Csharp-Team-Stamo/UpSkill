bf0475102fa66246fcdcc774d1ab5ba56263e90f
Nel <n.ifandieva@gmail.com>
2021-10-29 18:21:37 +0300

    An empty commit

--- UpSkill/Services/UpSkill.Services.Data/EmailSender.cs
+++ UpSkill/Services/UpSkill.Services.Data/EmailSender.cs
@@ -59,7 +59,7 @@
             var result = await client.SendEmailAsync(email);
 
             //TODO Impelment Error Handling
-
+            // a small change to be able to commit
             return result;
         }
     }
