1f577f29d1c581f1d80b0370a5df6618d0d915da
Nel <n.ifandieva@gmail.com>
2021-11-03 10:25:55 +0200

    Created Statistics Controller in the Admin Area

---
+++ UpSkill/Api/Areas/Admin/Controllers/StatisticsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UpSkill.Api.Areas.Admin.Controllers
+{
+    public class StatisticsController : AdminController
+    {
+        //public async Task<ActionResult<IEnumerable<>>> UpSkillClients()
+        //{
+        //    return new List<>;
+        //}
+    }
+}
---
+++ UpSkill/Infrastructure/UpSkill.Infrastructure.Models/Admin/MonthlyClient.cs
@@ -0,0 +1,15 @@
+﻿namespace UpSkill.Infrastructure.Models.Admin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class MonthlyClient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
