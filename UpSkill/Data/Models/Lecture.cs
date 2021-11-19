27e5f08ebc9bb6ad3431d5b5393556d9b60afb18
Nel <n.ifandieva@gmail.com>
2021-11-19 21:26:54 +0200

    Created Lecture, LectureResources and a Migration

---
+++ UpSkill/Data/Models/Lecture.cs
@@ -0,0 +1,20 @@
+﻿namespace UpSkill.Data.Models
+{
+    using System.Collections.Generic;
+    using UpSkill.Data.Common.Models;
+
+    public class Lecture : BaseDeletableModel<int>
+    {
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public string VideoUrl { get; set; }
+
+        public ICollection<LectureResource> Resources { get; set; } =
+            new List<LectureResource>();
+    }
+}
