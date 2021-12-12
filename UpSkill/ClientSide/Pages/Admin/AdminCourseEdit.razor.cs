169117431aad59e995d8faf4bb6f478153b41884
Nel <n.ifandieva@gmail.com>
2021-12-12 18:55:44 +0200

    Fixed AdminCourseEdit functionality

--- UpSkill/ClientSide/Pages/Admin/AdminCourseEdit.razor.cs
+++ UpSkill/ClientSide/Pages/Admin/AdminCourseEdit.razor.cs
@@ -6,6 +6,7 @@
     using Microsoft.AspNetCore.Components;
     using UpSkill.Infrastructure.Models.Category;
     using UpSkill.Infrastructure.Models.Course;
+    using UpSkill.Infrastructure.Models.Language;
 
     public partial class AdminCourseEdit : ComponentBase
     {
@@ -17,6 +18,9 @@
         public IEnumerable<AdminCategoryListingServiceModel> CategoriesInDb { get; set; } =
             new List<AdminCategoryListingServiceModel>();
 
+        public IEnumerable<LanguageListingServiceModel> LanguagesInDb { get; set; } =
+            new List<LanguageListingServiceModel>();
+
         protected override async Task OnInitializedAsync()
         {
             this.editModel = await this.Client
@@ -26,6 +30,10 @@
             this.CategoriesInDb = await this.Client
                 .GetFromJsonAsync<IEnumerable<AdminCategoryListingServiceModel>>
                 ("/admin/category/all");
+
+            this.LanguagesInDb = await this.Client
+                .GetFromJsonAsync<IEnumerable<LanguageListingServiceModel>>
+                ("/admin/language/all");
         }
 
         public async Task Edit()
--- UpSkill/Infrastructure/UpSkill.Infrastructure.Models/Course/CourseEditInputModel.cs
+++ UpSkill/Infrastructure/UpSkill.Infrastructure.Models/Course/CourseEditInputModel.cs
@@ -5,33 +5,96 @@
     public class CourseEditInputModel
     {
         public int Id { get; set; }
+        private const string RequiredErrorMessage = " is required.";
+        private const int NameMinLen = 5;
+        private const int NameMaxLen = 40;
+        private const int DescriptionMinLen = 10;
+        private const int DescriptionMaxLen = 150;
+        private const int AuthorNameMinLen = 2;
+        private const int AuthorNameMaxLen = 30;
+        private const int AuthorCompanyNameMinLen = 2;
+        private const int AuthorCompanyNameMaxLen = 30;
+        private const int DurationMinLen = 1;
+        private const int DurationMaxLen = 3;
+        private const int LectureNumMinLen = 1;
+        private const int LecturesNumMaxLen = 3;
+        private const int SkillsMinLen = 10;
+        private const int SkillsMaxLen = 150;
+
+        [Required]
+        [Range(1, 100, ErrorMessage = "Category" + RequiredErrorMessage)]
+        [Display(Name = "Category Id")]
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
 
-        //public IEnumerable<AdminCategoryListingServiceModel> AllCategories { get; set; } =
-        //    new List<AdminCategoryListingServiceModel>();
-
-        [Required]
+        [Required(ErrorMessage = nameof(Name) + RequiredErrorMessage)]
+        [MinLength(NameMinLen)]
+        [MaxLength(NameMaxLen)]
         public string Name { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = nameof(Description) + RequiredErrorMessage)]
+        [MinLength(DescriptionMinLen)]
+        [MaxLength(DescriptionMaxLen)]
         public string Description { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = nameof(ImageUrl) + RequiredErrorMessage)]
+        [Url]
+        [Display(Name = "Image URL")]
         public string ImageUrl { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = nameof(AuthorFullName) + RequiredErrorMessage)]
+        [MinLength(AuthorNameMinLen)]
+        [MaxLength(AuthorNameMaxLen)]
+        [Display(Name = "Author Full Name")]
         public string AuthorFullName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = nameof(AuthorImageUrl) + RequiredErrorMessage)]
+        [Url]
+        [Display(Name = "Author Image URL")]
+        public string AuthorImageUrl { get; set; }
+
+        [Required(ErrorMessage = nameof(CompanyLogoUrl) + RequiredErrorMessage)]
+        [Url]
         [Display(Name = "Author Company Logo URL")]
         public string CompanyLogoUrl { get; set; }
 
-        [Range(0.00, 1000.00)]
+        [Required(ErrorMessage = nameof(CompanyName) + RequiredErrorMessage)]
+        [MinLength(AuthorCompanyNameMinLen)]
+        [MaxLength(AuthorCompanyNameMaxLen)]
+        [Display(Name = "Author Company Name")]
+        public string CompanyName { get; set; }
+
+        [Required(ErrorMessage = nameof(DurationInHours) + RequiredErrorMessage)]
+        [MinLength(DurationMinLen)]
+        [MaxLength(DurationMaxLen)]
+        [Display(Name = "Duration In Hours")]
+        public string DurationInHours { get; set; }
+
+        [Required(ErrorMessage = nameof(LecturesCount) + RequiredErrorMessage)]
+        [MinLength(LectureNumMinLen)]
+        [MaxLength(LecturesNumMaxLen)]
+        [Display(Name = "Lectures Count")]
+        public string LecturesCount { get; set; }
+
+        [Required(ErrorMessage = nameof(SkillsLearn) + RequiredErrorMessage)]
+        [MinLength(SkillsMinLen)]
+        [MaxLength(SkillsMaxLen)]
+        [Display(Name = "Skills To Learn")]
+        public string SkillsLearn { get; set; }
+
+        [Range(typeof(decimal), "0", "9999")]
         [DataType(DataType.Currency)]
         public decimal Price { get; set; }
 
         [Required]
+        [Range(1, 100, ErrorMessage = "Language" + RequiredErrorMessage)]
+        [Display(Name = "Language Id")]
+        public int LanguageId { get; set; }
+        public string LanguageName { get; set; }
+
+        [Required(ErrorMessage = nameof(VideoUrl) + RequiredErrorMessage)]
+        [Url]
+        [Display(Name = "Video URL")]
         public string VideoUrl { get; set; }
     }
 }
